e2f34d7f7bbeae9fe73c52984ab8e73bf94ed9ae
Reed Taylor <rtaylor@umail.ucsb.edu>
2019-05-06 13:43:53 -0700

    Minor changes and code clean up

--- AndroidApp/app/src/main/java/com/cloudcontrol/remote/MainActivity.java
+++ AndroidApp/app/src/main/java/com/cloudcontrol/remote/MainActivity.java
@@ -12,7 +12,6 @@ first digit: 1 for tablet, 0 for drone
 
 package com.cloudcontrol.remote;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.Color;
@@ -39,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
     private static final int RECORDER_CHANNELS = AudioFormat.CHANNEL_IN_MONO;
     private static final int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
     private AudioRecord recorder = null;
-    private Thread sendingThread = null;
+    private Thread recordingThread = null;
     private Thread receivingThread = null;
     private boolean recording = false;
     public static final String SERVER_IP = "192.168.42.1"; //server IP address
@@ -71,16 +70,10 @@ public class MainActivity extends AppCompatActivity {
         recordingButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!recording) {
-                    recordingButton.setText("Stop Recording");
-                    recordingButton.setCompoundDrawablesWithIntrinsicBounds(null, mContext.getResources().getDrawable(R.drawable.square), null, null);
-                    startRecording();
-                }
-                else {
-                    recordingButton.setText("Start Recording");
-                    recordingButton.setCompoundDrawablesWithIntrinsicBounds(null, mContext.getResources().getDrawable(R.drawable.circle), null, null);
-                    stopRecording();
-                }
+                if (!recording)
+                    startRecording(true);
+                else
+                    stopRecording(true);
             }
         });
     }
@@ -95,9 +88,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-        final Button recordingButton = this.findViewById(R.id.button);
-        recordingButton.setText("Start Recording");
-        stopRecording();
+        //Stop recording nd send disconnect signal
+        stopRecording(true);
         try {
             gcsUplink.setText("GCS Uplink: Down");
             gcsUplink.setTextColor(Color.RED);
@@ -107,128 +99,126 @@ public class MainActivity extends AppCompatActivity {
         catch (IOException e) {}
     }
 
-    private void startRecording() {
+    private void startRecording(Boolean tablet) {
+        // Changes UI for recording and sends recording signal if originating from tablet
         //TODO: implement using microphone
-        recording = true;
         try{
-            socket.getOutputStream().write("10".getBytes());
+            if (tablet)
+                socket.getOutputStream().write("10".getBytes());
+            recording = true;
+            recordingButton.post(new Runnable() {
+                @Override
+                public void run() {
+                    recordingButton.setText("Stop Recording");
+                    recordingButton.setCompoundDrawablesWithIntrinsicBounds(null, mContext.getResources().getDrawable(R.drawable.square), null, null);
+                }
+            });
         }
         catch (IOException e) {}
     }
 
-    private void stopRecording() {
-        recording = false;
+    private void stopRecording(Boolean tablet) {
+        // Changes UI for recording and sends recording signal if originating from tablet
         try{
-            socket.getOutputStream().write("11".getBytes());
+            if (tablet)
+                socket.getOutputStream().write("11".getBytes());
+            recording = false;
+            recordingButton.post(new Runnable() {
+                @Override
+                public void run() {
+                    recordingButton.setText("Start Recording");
+                    recordingButton.setCompoundDrawablesWithIntrinsicBounds(null, mContext.getResources().getDrawable(R.drawable.circle), null, null);
+                }
+            });
         }
         catch (IOException e) {}
+    }
+
+    private void setDroneUplink(final Boolean up) {
+        //Update UI for Drone uplink status
+        droneUplink.post(new Runnable() {
+            @Override
+            public void run() {
+                if (up) {
+                    droneUplink.setText("Drone Uplink: Up");
+                    droneUplink.setTextColor(Color.rgb(0,100,0));
+                }
+                else {
+                    droneUplink.setText("Drone Uplink: Down");
+                    droneUplink.setTextColor(Color.RED);
+                }
+            }
+        });
+    }
+
+    private void setGcsUplink(final Boolean up) {
+        //Update UI for GCS uplink status
+        gcsUplink.post(new Runnable() {
+            @Override
+            public void run() {
+                if (up) {
+                    gcsUplink.setText("GCS Uplink: Up");
+                    gcsUplink.setTextColor(Color.rgb(0,100,0));
+                }
+                else {
+                    gcsUplink.setText("GCS Uplink: Down");
+                    gcsUplink.setTextColor(Color.RED);
+                }
+            }
+        });
+    }
+
+    private void setFailedPackets(final String count) {
+        //Update UI for failed packet count
+        failedPackets.post(new Runnable() {
+            @Override
+            public void run() {
+                failedPackets.setText("Failed Packets: " + count);
+            }
+        });
     }
 
     private void receiveUpdates() {
+        //Listener for receiving messages from GCS
         while (true) {
             try {
                 dataIn = new DataInputStream(socket.getInputStream());
                 String serverMessage = dataIn.readLine();
                 switch (serverMessage.substring(0,2)) {
                     case "00":
-                        recording = true;
-                        recordingButton.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                recordingButton.setText("Stop Recording");
-                                recordingButton.setCompoundDrawablesWithIntrinsicBounds(null, mContext.getResources().getDrawable(R.drawable.square), null, null);
-                            }
-                        });
+                        startRecording(false);
                         break;
                     case "01":
-                        recording = false;
-                        recordingButton.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                recordingButton.setText("Start Recording");
-                                recordingButton.setCompoundDrawablesWithIntrinsicBounds(null, mContext.getResources().getDrawable(R.drawable.circle), null, null);
-                            }
-                        });
+                        stopRecording(false);
                         break;
-                    case "02":
-                        if (serverMessage.charAt(2) == '1') {
-                            droneUplink.post(new Runnable() {
-                                @Override
-                                public void run() {
-                                    droneUplink.setText("Drone Uplink: Up");
-                                    droneUplink.setTextColor(Color.rgb(0,100,0));
-                                }
-                            });
-                        }
-                        else {
-                            droneUplink.post(new Runnable() {
-                                @Override
-                                public void run() {
-                                    droneUplink.setText("Drone Uplink: Down");
-                                    droneUplink.setTextColor(Color.RED);
-                                }
-                            });
-                        }
+                    case "02": //update drone status
+                        if (serverMessage.charAt(2) == '1')
+                            setDroneUplink(true);
+                        else
+                            setDroneUplink(false);
                         break;
                     case "03":
-                        final String count = serverMessage.substring(2, serverMessage.length()-1);
-                        failedPackets.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                failedPackets.setText("Failed Packets: " + count);
-                            }
-                        });
+                        setFailedPackets(serverMessage.substring(2, serverMessage.length()-1));
                         break;
                     case "04":
                         Log.d("WIFI", "Connected to GCS!!");
-                        gcsUplink.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                gcsUplink.setText("GCS Uplink: Up");
-                                gcsUplink.setTextColor(Color.rgb(0,100,0));
-                            }
-                        });
+                        setGcsUplink(true);
                         break;
                     default:
                         throw new IOException();
                 }
-//                Log.d("WIFI", "Message received: " + serverMessage);
             }
             catch (Exception e) {
                 Log.d("WIFI", "couldn't receive packet");
-                gcsUplink.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        gcsUplink.setText("GCS Uplink: Down");
-                        gcsUplink.setTextColor(Color.RED);
-                    }
-                });
-                droneUplink.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        droneUplink.setText("Drone Uplink: Down");
-                        droneUplink.setTextColor(Color.RED);
-                    }
-                });
+                setGcsUplink(false);
+                setDroneUplink(false);
                 break;
             }
         }
         try {
             Log.d("WIFI", "closing");
-            gcsUplink.post(new Runnable() {
-                @Override
-                public void run() {
-                    gcsUplink.setText("GCS Uplink: Down");
-                    gcsUplink.setTextColor(Color.RED);
-                }
-            });
-            droneUplink.post(new Runnable() {
-                @Override
-                public void run() {
-                    droneUplink.setText("Drone Uplink: Down");
-                    droneUplink.setTextColor(Color.RED);
-                }
-            });
+            setGcsUplink(false);
+            setDroneUplink(false);
             socket.close();
         }
         catch (Exception e) {}
@@ -236,20 +226,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void showAlert(final String title, final String message) {
+        //Show failed alert that restarts wifi setup on continue
         MainActivity.this.runOnUiThread(new Runnable() {
             @Override
             public void run() {
                 new AlertDialog.Builder(mContext)
-                        .setTitle(title)
-                        .setMessage(message)
-                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int which) { connectToWifi(); }})
-                        .show();
+                    .setTitle(title)
+                    .setMessage(message)
+                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) { connectToWifi(); }})
+                    .show();
             }
         });
     }
 
     private void connectToWifi() {
+        // Logic for connecting to WIFI network on RPi
         WifiManager wifiManager = (WifiManager) mContext.getSystemService(Context.WIFI_SERVICE);
         WifiConfiguration cgsConfig = new WifiConfiguration();
         if (wifiManager.getConnectionInfo().getSSID().equals("\"CloudControlGCS\"") && wifiManager.getConnectionInfo().getSupplicantState().toString() == "COMPLETED") {
@@ -295,6 +287,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void startTCP() {
+        //Initialize receiving thread
         receivingThread = new Thread(new Runnable() {
             public void run() {
                 int connectTries = 0;
@@ -302,13 +295,7 @@ public class MainActivity extends AppCompatActivity {
                     try {
                         socket = new Socket(SERVER_IP, SERVER_PORT);
                         Log.d("WIFI", "Connected to RPI");
-                        gcsUplink.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                gcsUplink.setText("GCS Uplink: Up");
-                                gcsUplink.setTextColor(Color.rgb(0,100,0));
-                            }
-                        });
+                        setGcsUplink(true);
                         break;
                     } catch (IOException e) {
                         if (connectTries++ > 10) {
